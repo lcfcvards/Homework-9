37ec5ebf323e441e552e720b8832431a476f5948
lcfcvards <sytnik.dmitry@gmail.com>
2022-10-25 00:59:34 +0300

    Homework 9.1

--- Homework 9.1.py
+++ Homework 9.1.py
@@ -1,7 +1,7 @@
 my_list = []
 
-for num in range(5):
-    N = int(input("Enter your number: "))
+for i in range(5):
+    N = int(input(f"Enter your number №{i + 1}: "))
     my_list.append(N)
 
 print(my_list)
